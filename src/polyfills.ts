e8df100b1880af7664bd76d02be8cbe03557ae7e
Nathan Pickard <nathan.pickard87@gmail.com>
2019-10-17 21:12:19 -0700

    Initializing amplify auth, api, and graphql schema

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -17,6 +17,8 @@
 /***************************************************************************************************
  * BROWSER POLYFILLS
  */
+(window as any).global = window; 
+(window as any).process = { browser: true };
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
